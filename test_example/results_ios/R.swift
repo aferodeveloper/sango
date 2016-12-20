12bfcfdb706a631b7b91265f67eaf50ad175516c
Steve Hales <steve@afero.io>
2016-12-20 14:36:39 -0800

    Update Sango tests

---
+++ test_example/results_ios/R.swift
@@ -0,0 +1,17 @@
+/* Generated with Sango, by Afero.io */
+
+import Foundation
+public struct R {
+	public struct String {
+		static let AppName = "app_name"
+		static let ButtonTitleCancel = "button_title_cancel"
+		static let ButtonTitleCancelAllCaps = "button_title_cancel_all_caps"
+		static let ButtonTitleOk = "button_title_ok"
+		static let DeviceOffline = "device_offline"
+		static let DialogMessageRemoveDevice = "dialog_message_remove_device"
+		static let DialogTitleRemoveDevice = "dialog_title_remove_device"
+		static let DialogTitleScheduleTime = "dialog_title_schedule_time"
+		static let NoNetworkWarning = "no_network_warning"
+		static let PrimaryAccountName = "primary_account_name"
+	}
+}
--- test_example/results_ios/Sango.swift
+++ test_example/results_ios/Sango.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 public struct Sango {
-    public static let Version = "Sango © 2016 Afero, Inc - Build 183"
+    public static let Version = "Sango © 2016 Afero, Inc - Build 186"
 }
 extension String {
     init(locKey key: String, value: String) {
