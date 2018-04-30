c82ba51a247a2b6ea8804d47f1ef9416061e8d9c
Steve Hales <steve@igorlabs.com>
2018-04-30 10:36:57 -0700

    Update test results

--- test_example/results_ios/Sango.swift
+++ test_example/results_ios/Sango.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 public struct Sango {
-    public static let Version = "Sango © 2016,2017 Afero, Inc - Build 245"
+    public static let Version = "Sango © 2016,2017 Afero, Inc - Build 258"
 }
 extension String {
     init(locKey key: String, value: String) {
